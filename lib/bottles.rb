c2f8062480cf675f3dfceba67ffcad16697a6800
Mary Rose Cook <mary@maryrosecook.com>
2017-05-11 13:32:22 +0100

    Verses and song work

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,7 +2,17 @@ require 'active_support/inflector'
 require_relative "./verse"
 
 class Bottles
+  def song
+    verses(99, 0)
+  end
+
   def verse(bottle_count)
     Verse.new(bottle_count).to_s
+  end
+
+  def verses(start, finish)
+    start.downto(finish).map do |bottle_count|
+      verse(bottle_count)
+    end.join("\n")
   end
 end
