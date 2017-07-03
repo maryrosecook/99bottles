9a83a4e336753336489db57b768b3abab98cb152
Mary Rose Cook <mary@maryrosecook.com>
2017-07-03 06:41:30 +0100

    Done

--- refactored/lib/bottles.rb
+++ refactored/lib/bottles.rb
@@ -1,7 +1,7 @@
 class Bottles
   def verse(number)
-    bottle_number = BottleNumber.new(number)
-    next_bottle_number = BottleNumber.new(bottle_number.successor)
+    bottle_number = BottleNumber.create(number)
+    next_bottle_number = bottle_number.successor
 
     "#{bottle_number}".capitalize + " of beer on the wall, " +
       "#{bottle_number} of beer.\n" +
@@ -25,47 +25,73 @@ class BottleNumber
     @number = number
   end
 
-  def successor
+  def self.create(number)
     if number == 0
-      99
+      BottleNumber0
+    elsif number == 1
+      BottleNumber1
+    elsif number == 6
+      BottleNumber6
     else
-      number - 1
-    end
+      BottleNumber
+    end.new(number)
+  end
+
+  def successor
+    BottleNumber.create(number - 1)
   end
 
   def action
-    if number == 0
-      "Go to the store and buy some more"
-    else
-      "Take #{pronoun} down and pass it around"
-    end
+    "Take #{pronoun} down and pass it around"
   end
 
   def container
-    if number === 1
-      "bottle"
-    else
-      "bottles"
-    end
+    "bottles"
   end
 
   def quantity
-    if number == 0
-      "no more"
-    else
-      number.to_s
-    end
+    number.to_s
   end
 
   def pronoun
-    if number == 1
-      "it"
-    else
-      "one"
-    end
+    "one"
   end
 
   def to_s
     "#{quantity} #{container}"
+  end
+end
+
+class BottleNumber0 < BottleNumber
+  def successor
+    BottleNumber.create(99)
+  end
+
+  def action
+    "Go to the store and buy some more"
+  end
+
+  def quantity
+    "no more"
+  end
+end
+
+class BottleNumber1 < BottleNumber
+  def container
+    "bottle"
+  end
+
+  def pronoun
+    "it"
+  end
+end
+
+class BottleNumber6 < BottleNumber
+  def container
+    "six-pack"
+  end
+
+  def quantity
+    1
   end
 end
